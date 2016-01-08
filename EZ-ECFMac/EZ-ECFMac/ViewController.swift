8b8d881b8985e1469cec49d4a0a853075f7842c2
Jon Slaughter <jonslaughter80123@gmail.com>
2016-01-07 19:49:14 -0700

    First Commit

---
+++ EZ-ECFMac/EZ-ECFMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EZ-ECFMac
+//
+//  Created by Jon Slaughter on 1/7/16.
+//  Copyright © 2016 Jon Slaughter. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
